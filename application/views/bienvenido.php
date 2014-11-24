656c861e7529fb74285365c18ef68ffdcbef6807
antonio <antoniopunzoboyzo@yahoo.com.mx>
2014-11-24 11:19:48 -0600

    Revert "empezamos"
    This reverts commit 43a0f2e3b592dcefa0a9db3e0ddd354de09a54d7.

--- application/views/bienvenido.php
+++
@@ -1,8 +0,0 @@
-  <div class="wrapper">  
-    <form action="<?php echo base_url('index.php/usuarios/acceso')?>" method="post" class='form-signin'>
-      <h2 class="form-signin-heading">Por favor Registrese</h2>
-      <input type="text" class="form-control" name="cve_usuario" placeholder="Usuario" required="" autofocus="" />
-      <input type="password" class="form-control" name="contrasena" placeholder="Contraseña" required=""/>            
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Ingresar</button>       
-    </form>
-  </div>
--- js/principal.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-        $('.nav>li').click(function(){
-                $('.nav>li').attr('class','');
-                $(this).attr('class','active');
-        });
-});
